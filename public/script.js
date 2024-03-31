abdefc1dbf8ea27f8948268965767fbcb804744f
Sree <sreepasumarthi21@gmail.com>
2024-03-31 11:45:17 -0400

    stuff

--- public/script.js
+++ public/script.js
@@ -1,144 +1,85 @@
-const getRecipes = async () => {
+const getCrafts = async () => {
     try {
-      return (await fetch("api/recipes/")).json();
+      return (await fetch("https://read-server-json-1.onrender.com/api/crafts")).json();
     } catch (error) {
-      console.log(error);
+      console.log("error retrieving data");
+      return "";
     }
   };
   
-  const showRecipes = async () => {
-    let recipes = await getRecipes();
-    let recipesDiv = document.getElementById("recipe-list");
-    recipesDiv.innerHTML = "";
-    recipes.forEach((recipe) => {
-      const section = document.createElement("section");
-      section.classList.add("recipe");
-      recipesDiv.append(section);
+  const openModal = (craft) => {
+    const modal = document.getElementById("myModal");
+    const modalTitle = document.getElementById("modal-title");
+    const modalDescription = document.getElementById("modal-description");
+    const modalSupplies = document.getElementById("modal-supplies");
+    const modalImage = document.getElementById("modal-image");
   
-      const a = document.createElement("a");
-      a.href = "#";
-      section.append(a);
+    modalTitle.innerHTML = `<strong>${craft.name}</strong>`;
+    modalDescription.textContent = craft.description;
   
-      const h3 = document.createElement("h3");
-      h3.innerHTML = recipe.name;
-      a.append(h3);
+    modalSupplies.innerHTML = "<strong>Supplies:</strong>";
+    craft.supplies.forEach((supply) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = supply;
+      modalSupplies.appendChild(listItem);
+    });
   
-      const img = document.createElement("img");
-      img.src = recipe.img;
-      a.append(img);
+    modalImage.src = "https://read-server-json-1.onrender.com/" + craft.img;
   
-      a.onclick = (e) => {
-        e.preventDefault();
-        displayDetails(recipe);
-      };
+    modal.style.display = "block";
+  
+    const closeModal = () => {
+      modal.style.display = "none";
+    };
+  
+    const closeButton = document.getElementsByClassName("close")[0];
+    closeButton.addEventListener("click", closeModal);
+  
+    window.addEventListener("click", (event) => {
+      if (event.target == modal) {
+        closeModal();
+      }
     });
   };
   
-  const displayDetails = (recipe) => {
-    openDialog("recipe-details");
-    const recipeDetails = document.getElementById("recipe-details");
-    recipeDetails.innerHTML = "";
-    recipeDetails.classList.remove("hidden");
+  const showCrafts = async () => {
+    const craftsJSON = await getCrafts();
+    const columns = document.querySelectorAll(".column");
   
-    const h3 = document.createElement("h3");
-    h3.innerHTML = recipe.name;
-    recipeDetails.append(h3);
-  
-    const p = document.createElement("p");
-    recipeDetails.append(p);
-    p.innerHTML = recipe.description;
-  
-    const ul = document.createElement("ul");
-    recipeDetails.append(ul);
-    console.log(recipe.ingredients);
-    recipe.ingredients.forEach((ingredient) => {
-      const li = document.createElement("li");
-      ul.append(li);
-      li.innerHTML = ingredient;
-    });
-  
-    const spoon = document.createElement("section");
-    spoon.classList.add("spoon");
-    recipeDetails.append(spoon);
-  };
-  
-  const addRecipe = async (e) => {
-    e.preventDefault();
-    const form = document.getElementById("add-recipe-form");
-    const formData = new FormData(form);
-    let response;
-    formData.append("ingredients", getIngredients());
-  
-    console.log(...formData);
-  
-    response = await fetch("/api/recipes", {
-      method: "POST",
-      body: formData,
-    });
-  
-    //successfully got data from server
-    if (response.status != 200) {
-      console.log("Error posting data");
-    }
-  
-    await response.json();
-    resetForm();
-    document.getElementById("dialog").style.display = "none";
-    showRecipes();
-  };
-  
-  const getIngredients = () => {
-    const inputs = document.querySelectorAll("#ingredient-boxes input");
-    let ingredients = [];
-  
-    inputs.forEach((input) => {
-      ingredients.push(input.value);
-    });
-  
-    return ingredients;
-  };
-  
-  const resetForm = () => {
-    const form = document.getElementById("add-recipe-form");
-    form.reset();
-    document.getElementById("ingredient-boxes").innerHTML = "";
-    document.getElementById("img-prev").src = "";
-  };
-  
-  const showRecipeForm = (e) => {
-    e.preventDefault();
-    openDialog("add-recipe-form");
-    resetForm();
-  };
-  
-  const addIngredient = (e) => {
-    e.preventDefault();
-    const section = document.getElementById("ingredient-boxes");
-    const input = document.createElement("input");
-    input.type = "text";
-    section.append(input);
-  };
-  
-  const openDialog = (id) => {
-    document.getElementById("dialog").style.display = "block";
-    document.querySelectorAll("#dialog-details > *").forEach((item) => {
-      item.classList.add("hidden");
-    });
-    document.getElementById(id).classList.remove("hidden");
-  };
-  
-  //initial code
-  showRecipes();
-  document.getElementById("add-recipe-form").onsubmit = addRecipe;
-  document.getElementById("add-link").onclick = showRecipeForm;
-  document.getElementById("add-ingredient").onclick = addIngredient;
-  
-  document.getElementById("img").onchange = (e) => {
-    if (!e.target.files.length) {
-      document.getElementById("img-prev").src = "";
+    if (craftsJSON == "") {
+      columns.forEach(column => {
+        column.innerHTML = "Sorry, no crafts";
+      });
       return;
     }
-    document.getElementById("img-prev").src = URL.createObjectURL(
-      e.target.files.item(0)
-    );
+  
+    let columnIndex = 0;
+    let columnCount = columns.length;
+    let columnHeights = Array.from(columns).map(() => 0); // Array to store column heights
+  
+    craftsJSON.forEach((craft, index) => {
+      // Find the shortest column
+      const shortestColumnIndex = columnHeights.indexOf(Math.min(...columnHeights));
+  
+      const galleryItem = document.createElement("div");
+      galleryItem.classList.add("gallery-item");
+      const img = document.createElement("img");
+      img.src = "https://read-server-json-1.onrender.com/" + craft.img;
+      img.alt = craft.name;
+      img.addEventListener("click", () => openModal(craft));
+      galleryItem.appendChild(img);
+      columns[shortestColumnIndex].appendChild(galleryItem);
+  
+      // Update the height of the column
+      columnHeights[shortestColumnIndex] += galleryItem.offsetHeight;
+  
+      // If the column's height exceeds the container's height, switch to the next column
+      if (columnHeights[shortestColumnIndex] >= columns[shortestColumnIndex].offsetHeight) {
+        columnIndex++;
+        if (columnIndex === columnCount) columnIndex = 0;
+        columnHeights[shortestColumnIndex] = 0; // Reset the height of the column
+      }
+    });
   };
+  
+  showCrafts();
--- server.js
+++ server.js
@@ -1,280 +1,353 @@
 const express = require("express");
 const app = express();
 const Joi = require("joi");
+const multer = require("multer");
 app.use(express.static("public"));
 app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/images/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 let crafts = [
-    {
-        name: "Beaded Jellyfish",
-        description: "Create a hanging jellyfish using egg cartons and multicolored beads",
-        supplies: [
-            "string",
-            "egg cartons",
-            "beads"
-        ],
-        img: "images/bead-jellyfish.jpg"
-    },
-    {
-        name: "Character Bookmarks",
-        description: "Create a little birdy bookmark to always remind you where you were",
-        supplies: [
-            "yellow construction paper",
-            "orange construction paper",
-            "black construction paper"
-        ],
-        img: "images/bookmarks.jpeg"
-    },
-    {
-        name: "Button Flowers",
-        description: "Create a fun bouquet of flowers with your favorite buttons",
-        supplies: [
-            "multicolored buttons",
-            "multicolored felt",
-            "green straws",
-            "ribbon"
-        ],
-        img: "images/button-flowers.jpeg"
-    },
-    {
-        name: "Cheerio Necklaces",
-        description: "Create a fun and edible necklace",
-        supplies: [
-            "Cheerios or Fruit Loops",
-            "Elastic string"
-        ],
-        img: "images/cheerio-necklace.webp"
-    },
-    {
-        name: "Cotton Ball Cupcakes",
-        description: "Decorate your fun-filled cupcake however you want.",
-        supplies: [
-            "Construction Paper",
-            "Cotton Balls",
-            "Black Sharpie",
-            "Glitter"
-        ],
-        img: "images/cotton-ball-cupcakes.webp"
-    },
-    {
-        name: "School Themed Mason Jars",
-        description: "Let's make mason jars too",
-        supplies: [
-            "Construction Paper",
-            "Cotton Balls",
-            "Black Sharpie",
-            "Glitter"
-        ],
-        img: "images/decorated-jars.jpeg"
-    },
-    {
-        name: "Egg Carton Flowers",
-        description: "Make a beautiful bouquet with egg cartons and other items you can find around the house",
-        supplies: [
-            "Egg Cartons",
-            "Buttons",
-            "Green Pipe Cleaner",
-            "Ribbon",
-            "Canvas"
-        ],
-        img: "images/egg-carton-flowers.jpg"
-    },
-    {
-        name: "Finger Puppets",
-        description: "These little critters are easy to make, and will entertain your little one while they make a show.",
-        supplies: [
-            "Pom-poms",
-            "Googly Eyes",
-            "Pipe Cleaner"
-        ],
-        img: "images/finger-puppets.jpeg"
-    },
-    {
-        name: "Ribbon Flower Headbands",
-        description: "Let your little one show off her new style with these pretty and customizable headbands",
-        supplies: [
-            "Plain headband",
-            "Ribbon",
-            "Buttons",
-            "Gems"
-        ],
-        img: "images/flower-headbands.jpg"
-    },
-    {
-        name: "Hand Print Fish Puppets",
-        description: "We all need to take every opportunity we can to remember those tiny hands, and what better way to do it, then to make fish puppets!",
-        supplies: [
-            "Popsicle sticks",
-            "Cardstock",
-            "Gems",
-            "Googly Eyes"
-        ],
-        img: "images/handprint-fish.jpg"
-    },
-    {
-        name: "Hand Print Tree",
-        description: "This is a fun way to get your little one into finger painting.",
-        supplies: [
-            "Watercolor Paper",
-            "Finger paint"
-        ],
-        img: "images/hand-print-tree.jpeg"
-    },
-    {
-        name: "Melted Bead Bowl",
-        description: "All they need to do is shape their favorite design, warm it up and they have a brand new bowl.",
-        supplies: [
-            "Beads",
-            "Bowl",
-            "Parchment paper"
-        ],
-        img: "images/melted-bead-bowl.jpeg"
-    },
-    {
-        name: "Monster Kites",
-        description: "Let's make those scary toilet paper rolls fly!",
-        supplies: [
-            "Toilet paper rolls",
-            "Paint",
-            "Tissue Paper",
-            "String"
-        ],
-        img: "images/monster-rolls.jpg"
-    },
-    {
-        name: "Pool Noodle Boats",
-        description: "Let's make a boat that will actually float, due to the floating bottom of a pool noodle.",
-        supplies: [
-            "Pool Noodle",
-            "Straw",
-            "Plastic Paper"
-        ],
-        img: "images/noodle-boats.png"
-    },
-    {
-        name: "Paper Plate Bees",
-        description: "Let's have fun with making cute little bees, or big bees actually.",
-        supplies: [
-            "Paper Plate",
-            "Googly Eyes",
-            "Close Pins",
-            "Black pom poms",
-            "Yellow Paint",
-            "Black Paint"
-        ],
-        img: "images/paper-plate-bees.jpeg"
-    },
-    {
-        name: "Paper Plate Dinosaurs",
-        description: "Who would have thought that half a paper plate would be the base of a dinosaur.",
-        supplies: [
-            "Paper Plate",
-            "Paint",
-            "Close Pins",
-            "Construction Paper"
-        ],
-        img: "images/paper-plate-dinosaurs.jpg"
-    },
-    {
-        name: "Porcupine Leaves",
-        description: "Let's turn an ordinary paper plate into a fun filled mask.",
-        supplies: [
-            "Leafs",
-            "Berries",
-            "Acorns",
-            "Construction Paper"
-        ],
-        img: "images/porcupine-leaf.webp"
-    },
-    {
-        name: "Rainbow Cloud",
-        description: "Some cotton and color and you'll have a beautiful rainbow.",
-        supplies: [
-            "Paper Plate",
-            "Cotton Balls",
-            "Construction Paper"
-        ],
-        img: "images/rainbow-cloud.webp"
-    },
-    {
-        name: "Fun Shaped Crayons",
-        description: "Let's melt some crayons together and let them harden into fun shapes.",
-        supplies: [
-            "Broken Crayons",
-            "Mold"
-        ],
-        img: "images/shaped-crayons.jpg"
-    },
-    {
-        name: "Straw Ferris Wheel",
-        description: "It might be too small to ride, but this farris wheel is the most colorful of all.",
-        supplies: [
-            "Multicolored straws",
-            "Platform"
-        ],
-        img: "images/straw-faris-wheel.jpg"
-    },
-    {
-        name: "Sunny String",
-        description: "Let's practice our fine motor skills while we weave the string into a fun sun.",
-        supplies: [
-            "Yellow String",
-            "Paper Plate",
-            "Yellow construction paper",
-            "Yellow and Orange beads"
-        ],
-        img: "images/sun-string.jpg"
-    },
-    {
-        name: "Tissue Ballerinas",
-        description: "These beautiful dancers will look great on display",
-        supplies: [
-            "Pipe cleaner",
-            "Tissue Paper",
-            "Elastics"
-        ],
-        img: "images/tissue-dancer.jpeg"
-    },
-    {
-        name: "Toilet Paper Roll Animals",
-        description: "Let's make those scary toilet paper rolls into cute animals!",
-        supplies: [
-            "Toilet Paper Rolls",
-            "Construction Paper",
-            "Googly Eyes"
-        ],
-        img: "images/toilet-paper-animals.jpeg"
-    },
-    {
-        name: "Toilet Paper Butterfly",
-        description: "Such a sweet little flyer",
-        supplies: [
-            "Toilet Paper Rolls",
-            "Construction Paper",
-            "Googly Eyes",
-            "Buttons"
-        ],
-        img: "images/toilet-paper-butterfly.jpg"
-    },
-    {
-        name: "Valentines Jar",
-        description: "So many hearts all in one",
-        supplies: [
-            "Clay",
-            "Glitter"
-        ],
-        img: "images/valentines-jar.webp"
-    }
+  {
+    _id: 1,
+    name: "Beaded Jellyfish",
+    description: "Create a hanging jellyfish using eggcartons and multicolored beads",
+    supplies: [
+      "string",
+      "egg cartons",
+      "beads"
+    ],
+    img: "images/bead-jellyfish.jpg"
+  },
+  {
+    _id: 2,
+    name: "Character Bookmarks",
+    description: "Create a little birdy bookmark to always remin you were you were",
+    supplies: [
+      "yellow construction paper",
+      "orange construction paper",
+      "black construction paper"
+    ],
+    img: "images/bookmarks.jpeg"
+  },
+  {
+    _id: 3,
+    name: "Button Flowers",
+    description: "Create a fun bouquet of flowers with your favorite buttons",
+    supplies: [
+      "multicolored buttons",
+      "multicolored flet",
+      "green straws",
+      "ribbon"
+    ],
+    img: "images/button-flowers.jpeg"
+  },
+  {
+    _id: 4,
+    name: "Cheerio Necklaces",
+    description: "Create a fun and edible necklace",
+    supplies: [
+      "Cheerios or Fruit Loops",
+      "Elastic string"
+    ],
+    img: "images/cheerio-necklace.webp"
+  },
+  {
+    _id: 5,
+    name: "Cotton Ball Cupcakes",
+    description: "Decorate your fun filled cupcake however you want.",
+    supplies: [
+      "Construction Paper",
+      "Cotton Balls",
+      "Black Sharpie",
+      "Glitter"
+    ],
+    img: "images/cotton-ball-cupcakes.webp"
+  },
+  {
+    _id: 6,
+    name: "School Themed Mason Jars",
+    description: "Let's make mason jars too",
+    supplies: [
+      "Construction Paper",
+      "Cotton Balls",
+      "Black Sharpie",
+      "Glitter"
+    ],
+    img: "images/decorated-jars.jpeg"
+  },
+  {
+    _id: 7,
+    name: "Egg Carton Flowers",
+    description: "Make a beautiful bouquet with egg cartons and other items you can find around the house",
+    supplies: [
+      "Egg Cartons",
+      "Butons",
+      "Green Pipe Cleaner",
+      "Ribbon",
+      "Canvas"
+    ],
+    img: "images/egg-carton-flowers.jpg"
+  },
+  {
+    _id: 8,
+    name: "Finger Puppets",
+    description: "These little critters are easy to make, and will entertain your little one while they make a show.",
+    supplies: [
+      "Pom-poms",
+      "Googly Eyes",
+      "Pipe Cleaner"
+    ],
+    img: "images/finger-puppets.jpeg"
+  },
+  {
+    _id: 9,
+    name: "Ribbon Flower Headbands",
+    description: "Let your little one show off her new style with these pretty and customizable headbands",
+    supplies: [
+      "Plain headband",
+      "Ribbon",
+      "Buttons",
+      "Gems"
+    ],
+    img: "images/flower-headbands.jpg"
+  },
+  {
+    _id: 10,
+    name: "Hand Print Fish Puppets",
+    description: "We all need to take every opportunity we can to remember those tiny hands, and what better way to do it, then to make fish puppets!",
+    supplies: [
+      "Popsicle sticks",
+      "Cardstock",
+      "Gems",
+      "Googly Eyes"
+    ],
+    img: "images/handprint-fish.jpg"
+  },
+  {
+    _id: 11,
+    name: "Hand Print Tree",
+    description: "This is a fun way to get your little one into finger painting.",
+    supplies: [
+      "Watercolor Paper",
+      "Finger paint"
+    ],
+    img: "images/hand-print-tree.jpeg"
+  },
+  {
+    _id: 12,
+    name: "Melted Bead Bowl",
+    description: "All they need to do is shape their faviorte design, warm it up and they have a brand new bowl.",
+    supplies: [
+      "Beads",
+      "Bowl",
+      "Parchment paper"
+    ],
+    img: "images/melted-bead-bowl.jpeg"
+  },
+  {
+    _id: 13,
+    name: "Monster Kites",
+    description: "Let's make those scary toilet paper rolls fly!",
+    supplies: [
+      "Toilet paper rolls",
+      "Paint",
+      "Tissue Paper",
+      "String"
+    ],
+    img: "images/monster-rolls.jpg"
+  },
+  {
+    _id: 14,
+    name: "Pool Noodle Boats",
+    description: "Let's make a boat that will actually float, due to the floating bottom of a pool noodle.",
+    supplies: [
+      "Pool Noodle",
+      "Straw",
+      "Plastic Paper"
+    ],
+    img: "images/noodle-boats.png"
+  },
+  {
+    _id: 15,
+    name: "Paper Plate Bees",
+    description: "Let's have fun with making cute little bees, or big bees actually.",
+    supplies: [
+      "Paper Plate",
+      "Googly Eyes",
+      "Close Pins",
+      "Black pom poms",
+      "Yellow Paint",
+      "Black Paint"
+    ],
+    img: "images/paper-plate-bees.jpeg"
+  },
+  {
+    _id: 16,
+    name: "Paper Plate Dinosaurs",
+    description: "Who would have thought that half a paper plate would be the base of a dinosaur.",
+    supplies: [
+      "Paper Plate",
+      "Paint",
+      "Close Pins",
+      "Construction Paper"
+    ],
+    img: "images/paper-plate-dinosaurs.jpg"
+  },
+  {
+    _id: 17,
+    name: "Porcupine Leaves",
+    description: "Let's turn an ordinary paper plate into a fun filled mask.",
+    supplies: [
+      "Leafs",
+      "Berries",
+      "Acorns",
+      "Construction Paper"
+    ],
+    img: "images/porcupine-leaf.webp"
+  },
+  {
+    _id: 18,
+    name: "Rainbow Cloud",
+    description: "Some cotton and color and you'll have a beautiful rainbow.",
+    supplies: [
+      "Paper Plate",
+      "Cotton Balls",
+      "Construction Paper"
+    ],
+    img: "images/rainbow-cloud.webp"
+  },
+  {
+    _id: 19,
+    name: "Fun Shaped Crayons",
+    description: "Let's melt some crayons together and let them harden into fun shapes.",
+    supplies: [
+      "Broken Crayons",
+      "Mold"
+    ],
+    img: "images/shaped-crayons.jpg"
+  },
+  {
+    _id: 20,
+    name: "Straw Ferris Wheel",
+    description: "It might be too small to ride, but this farris wheel is the most colorful of all.",
+    supplies: [
+      "Multicolored straws",
+      "Platform"
+    ],
+    img: "images/straw-faris-wheel.jpg"
+  },
+  {
+    _id: 21,
+    name: "Sunny String",
+    description: "Let's practice our fine motor skills while we weave the string into a fun sun.",
+    supplies: [
+      "Yellow String",
+      "Paper Plate",
+      "Yellow construction paper",
+      "Yellow and Orange beads"
+    ],
+    img: "images/sun-string.jpg"
+  },
+  {
+    _id: 22,
+    name: "Tissue Ballerinas",
+    description: "These beautiful dancers will look great on display",
+    supplies: [
+      "Pipe cleaner",
+      "Tissue Paper",
+      "Elastics"
+    ],
+    img: "images/tisue-dancer.jpeg"
+  },
+  {
+    _id: 23,
+    name: "Toilet Paper Roll Animals",
+    description: "These beautiful dancers will look great on display",
+    supplies: [
+      "Toilet Paper Rolls",
+      "Construction Paper",
+      "Googly Eyes"
+    ],
+    img: "images/toilet-paper-animals.jpeg"
+  },
+  {
+    _id: 24,
+    name: "Toilet Paper Butterfly",
+    description: "Such a sweet little flyer",
+    supplies: [
+      "Toilet Paper Rolls",
+      "Construction Paper",
+      "Googly Eyes",
+      "Buttons"
+    ],
+    img: "images/toilet-paper-butterfly.jpg"
+  },
+  {
+    _id: 25,
+    name: "Valentines Jar",
+    description: "So much hearts all in one",
+    supplies: [
+      "Clay",
+      "Glitter"
+    ],
+    img: "images/valentines-jar.webp"
+  },
 ];
 
-app.get("/api/crafts", (req,res)=>{
-    res.send(crafts);
+app.get("/api/crafts", (req, res) => {
+  res.send(crafts);
 });
 
-app.listen(3031, () => {
-    console.log("listening");
+app.post("/api/crafts", upload.single("img"), (req, res) => {
+  const result = validateCraft(req.body);
+
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const craft = {
+    _id: crafts.length + 1,
+    name: req.body.name,
+    description: req.body.description,
+    supplies: req.body.supplies.split(","),
+  };
+
+  if (req.file) {
+    craft.img = "images/" + req.file.filename;
+  }
+
+  crafts.push(craft);
+  res.json(crafts);
+});
+
+const validateCraft = (craft) => {
+  const schema = Joi.object({
+    _id: Joi.allow(""),
+    supplies: Joi.allow(""),
+    name: Joi.string().min(3).required(),
+    description: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(craft);
+};
+
+app.listen(3030, () => {
+  console.log("listening");
 });
